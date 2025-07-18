ff5f8c95f739d7a16f5f6160187bf5107fc619a1
Reentryti <reentryti5@outlook.fr>
2025-07-18 06:31:27 +0000

    Model Training Pipeline

--- backend/capture/models.py
+++ backend/capture/models.py
@@ -57,6 +57,10 @@ class ZeekLog(models.Model):
 class Prediction(models.Model):
     session = models.ForeignKey(CaptureSession, on_delete=models.CASCADE, null= True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    src_ip = models.GenericIPAddressField(null=True, blank=True) #newly added
+    dst_ip = models.GenericIPAddressField(null=True, blank=True) #newly added
+    
     src_port = models.PositiveIntegerField()
     dst_port = models.PositiveIntegerField()
     proto = models.CharField(max_length=10)
